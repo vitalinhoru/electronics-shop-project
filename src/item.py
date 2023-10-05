29ca3c469ea10df2b61bc01e722bb7944b888b8b
vitalinho <kirillovitaly@mail.ru>
2023-10-05 19:24:08 +0300

    корректировка по Flake8

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -18,6 +19,7 @@ class Item:
         self.quantity = quantity
         Item.all.append(self)
 
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -26,6 +28,7 @@ class Item:
         """
         total_price = self.price * self.quantity
         return total_price
+
 
     def apply_discount(self) -> None:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -3,19 +3,19 @@ from src.item import Item
 
 
 def test_item():
-    item = Item(name='milk', price=80, quantity=10)
+    item = Item(name = 'milk', price = 80, quantity = 10)
     assert item.name == 'milk'
     assert item.price == 80
     assert item.quantity == 10
 
 
 def test_calculate_total_price():
-    item = Item(name='milk', price=80, quantity=10)
+    item = Item(name = 'milk', price = 80, quantity = 10)
     assert item.calculate_total_price() == 800
 
 
 def test_apply_discount():
-    item = Item(name='milk', price=80, quantity=10)
+    item = Item(name = 'milk', price = 80, quantity = 10)
     assert item.price == 80
     Item.pay_rate = 1.5
     assert item.apply_discount() is None
