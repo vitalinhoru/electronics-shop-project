d85c252bae4c1cd320ccf817d3081fafb58af7c1
vitalinho <kirillovitaly@mail.ru>
2023-10-08 19:13:52 +0300

    внесены изменения в класс Item, добавлены методы

--- src/item.py
+++ src/item.py
@@ -1,10 +1,13 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -14,11 +17,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
-
 
     def calculate_total_price(self) -> float:
         """
@@ -29,9 +31,48 @@ class Item:
         total_price = self.price * self.quantity
         return total_price
 
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @property
+    def name(self):
+        """
+        Getter для name
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Setter для name
+        """
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, filename):
+        """
+        Загружает данные из csv
+        """
+        Item.all.clear()
+        current_dir = os.path.dirname(__file__)  # возвращает имя директории пути path
+        root_dir = os.path.split(current_dir)[0]  # разбивает путь на кортеж (голова, хвост)
+        filepath = os.path.join(root_dir, filename)  # соединяет пути
+        if os.path.isfile(filepath):
+            with open(filepath, newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for i in reader:
+                    items = cls(name=i.get('name'), price=i.get('price'), quantity=i.get('quantity'))
+
+    @staticmethod
+    def string_to_number(str_number):
+        """
+        Возвращает число из числа-строки
+        """
+        number = int(float(str_number.replace(',', '.')))
+        return number
